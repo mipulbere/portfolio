ed898b665c49d9dc676f86a3131fdfc33b4210d1
Mirela Pulbere <mipulbere@gmail.com>
2017-07-31 18:28:48 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import Header from './components/headerComponents/header'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      <Header />
+
+      \</div>
+    );
+  }
+}
+
+export default App;
